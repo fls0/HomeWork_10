29eb9fd40c9af3de8d58191e145935151d4112b5
fls0 <134416720+fls0@users.noreply.github.com>
2023-08-09 15:08:55 +0200

    chang

--- classes_for_assistant.py
+++ classes_for_assistant.py
@@ -41,15 +41,15 @@ class Phone:
         self.value = phone
 
 
-
-name = Name('Bill')
-phone = Phone('1234567890')
-rec = Record(name, phone)
-ab = AddressBook()
-ab.add_record(rec)
-assert isinstance(ab['Bill'], Record)
-assert isinstance(ab['Bill'].name, Name)
-assert isinstance(ab['Bill'].phones, list)
-assert isinstance(ab['Bill'].phones[0], Phone)
-assert ab['Bill'].phones[0].value == '1234567890'
-print('All Ok)')
+if __name__ == "__main__":
+    name = Name('Bill')
+    phone = Phone('1234567890')
+    rec = Record(name, phone)
+    ab = AddressBook()
+    ab.add_record(rec)
+    assert isinstance(ab['Bill'], Record)
+    assert isinstance(ab['Bill'].name, Name)
+    assert isinstance(ab['Bill'].phones, list)
+    assert isinstance(ab['Bill'].phones[0], Phone)
+    assert ab['Bill'].phones[0].value == '1234567890'
+    print('All Ok)')
